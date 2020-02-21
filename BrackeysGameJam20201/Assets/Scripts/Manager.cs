254aacdecfcd67dfd54ee7f1545eae03e112712c
mlhomsi <mlhomsi@gmail.com>
2020-02-20 22:33:55 -0300

    Level System

--- BrackeysGameJam20201/Assets/Scripts/Manager.cs
+++ BrackeysGameJam20201/Assets/Scripts/Manager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class Manager : MonoBehaviour
     private int LiveBalls;
     public GameObject BallPrefabs;
     public Transform SpawnPoint;
+    public Text LevelText;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        LevelText.text = "Level: " + Level; 
         StartLevel();
     }
     // Update is called once per frame
